ab3b27a5be3f6446e26f5f1fd5c07d4e3be01a77
tristanmeyer <42872117+tristanmeyer@users.noreply.github.com>
2019-01-24 19:46:48 -0500

    Updated from Brython Server: 1/24/2019 7:46:48 PM

--- Multiplication-Problems.py
+++ Multiplication-Problems.py
@@ -1,1 +1,18 @@
+#Tristan Meyer
+#Multiplication Problems
+from random import randint
 
+ix = randint(1,10)
+iy = randint(1,10)
+x = str(ix)
+y = str(iy)
+
+answer = int(input("what is "+x+"*"+y+"? "))
+ranswer = ix*iy
+
+
+if ranswer == answer:
+    print("Correct!")
+    
+if ranswer != answer:
+    print("Incorrect, the answer was",ranswer)
