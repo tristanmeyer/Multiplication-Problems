3a68c94d55afd34406247876beaf7c8ef7736ba3
tristanmeyer <42872117+tristanmeyer@users.noreply.github.com>
2019-01-25 13:18:37 -0500

    Updated from Brython Server: 1/25/2019 1:18:37 PM

--- Multiplication-Problems.py
+++ Multiplication-Problems.py
@@ -4,21 +4,19 @@ from random import randint
 
 a = 0
 
-for b in range(1000):
-    if a < 5:
-        
-        ix = randint(1,10)
-        iy = randint(1,10)
-        x = str(ix)
-        y = str(iy)
-        answer = int(input("what is "+x+"*"+y+"? "))
-        ranswer = ix*iy
-        
-        if ranswer != answer:
-            print("Incorrect, the answer was", ranswer)
+while a < 5:
+    ix = randint(1,10)
+    iy = randint(1,10)
+    x = str(ix)
+    y = str(iy)
+    answer = int(input("what is "+x+"*"+y+"? "))
+    ranswer = ix*iy
+    
+    if ranswer != answer:
+        print("Incorrect, the answer was", ranswer)
 
-        if ranswer == answer:
-            print("Correct!")
-            a += 1 
-            
+    if ranswer == answer:
+        print("Correct!")
+        a += 1 
+        
 
